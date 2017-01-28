0e5750f85e9e011028a1704db16047c2d26da7ee
Maxence Frenette <maxence.frenette@gmail.com>
2017-01-28 14:29:40 -0500

    Setup dev environment

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+path = require('path');
+
+module.exports = {
+    entry: './main.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        loaders: [{
+            test: /\.ts$/, exclude: /node_modules/, loader: 'ts-loader'
+        }]
+    }
+};
