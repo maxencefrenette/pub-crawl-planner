3e70203eba17d82d5a86774df73d3f30331b8b6a
Maxime Plante <maxime.pl@outlook.com>
2017-01-29 00:07:50 -0500

    PDF code works with mockup data

--- js/pdfGenerator.js
+++ js/pdfGenerator.js
@@ -1,37 +1,37 @@
-//
-function pdfGenerator(numberOfTeams, numberOfLocations, combinations) {
+function pdfGenerator(numberOfTeams, locations, combinations) {
+
   this.numberOfTeams = numberOfTeams;
   this.numberOfLocations = numberOfLocations;
   this.combinations = combinations;
   var combinationsLength = 3; //TODO
 
-}
-function testPDF() {
-    /// create a document and pipe to a blob
-    var doc = new PDFDocument();
-    var stream = doc.pipe(blobStream());
+  /// create a document and pipe to a blob
+  var doc = new PDFDocument();
+  var stream = doc.pipe(blobStream());
 
-    doc.fontSize(12);
-    // doc.text('8', 300, 105);
+  doc.fontSize(12);
+  // doc.text('8', 300, 105);
 
-    doc.moveDown();
+  doc.moveDown();
 
-    doc.fontSize(24);
-    var i;
-    for (i = 1; i <= 5; i++) {
+  doc.fontSize(24);
+  var i;
+  for (i = 1; i <= 5; i++) {
       doc.text('Team '+i.toString(), {
-        widh:410,
-        align:'center'
+          align:'center'
       });
-    doc.addPage();
-    }
+      doc.moveDown(2);
+      doc.fontSize(12);
+      doc.text('8:05 - Gerts', {
+          align: 'left'
+      })
+      doc.addPage();
+  }
 
-
-
-
-    // end and display the document in the iframe to the right
-    doc.end();
-    stream.on('finish', function() {
+  // end and display the document in the iframe to the right
+  doc.end();
+  stream.on('finish', function() {
       location.href = stream.toBlobURL('application/pdf');
-    });
+  });
+
 }
