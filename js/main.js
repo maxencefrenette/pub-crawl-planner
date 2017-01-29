78001b2d7225e60c8a38c79dbfa51cc4c214caf0
Maxime Plante <maxime.pl@outlook.com>
2017-01-28 19:06:04 -0500

    Created the pub input

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function initMap() {
+    new google.maps.places.Autocomplete(document.getElementById('initial_location'));
+    new google.maps.places.Autocomplete(document.getElementById('final_location'));
+}
+
+$(document).ready(function() {
+    $('select').material_select();
+});
+
+
+$('#location_number').change(function(){
+    var location_number = $('#location_number').val();
+    var out = "";
+    for(var i=0;i<location_number;i++) {
+        if ($('#pub'+i)) {
+            var previous_content = $('#pub'+i).val();
+        }
+        out += '<div class="row">';
+        out += '   <div class="input-field col s12">';
+        if (previous_content) {
+            out += '        <input placeholder="" id="pub'+i+'" type="text" class="validate" value='+previous_content+'>';
+        } else {
+            out += '        <input placeholder="" id="pub'+i+'" type="text" class="validate">';
+        }
+        out += '        <label for="pub'+i+'">Pub '+(i+1)+' location</label>';
+        out += '   </div>';
+        out += '</div>';
+    }
+    $("#locations").html(out);
+    $('select').material_select();
+    for(var i=0;i<location_number;i++) {
+        new google.maps.places.Autocomplete(document.getElementById('pub'+i));
+        // Solves a bug where the input label would overlap the already present content
+        $('#pub'+i).focus();
+    }
+});
