492738241d12ac676e2c91b6e3ca0da2f80594c7
Maxime Plante <maxime.pl@outlook.com>
2017-01-28 23:59:18 -0500

    Hided pdf test code

--- js/pdfGenerator.js
+++ js/pdfGenerator.js
@@ -5,34 +5,33 @@ function pdfGenerator(numberOfTeams, numberOfLocations, combinations) {
   this.combinations = combinations;
   var combinationsLength = 3; //TODO
 
-
-
 }
+function testPDF() {
+    /// create a document and pipe to a blob
+    var doc = new PDFDocument();
+    var stream = doc.pipe(blobStream());
 
-/// create a document and pipe to a blob
-var doc = new PDFDocument();
-var stream = doc.pipe(blobStream());
+    doc.fontSize(12);
+    // doc.text('8', 300, 105);
 
-doc.fontSize(12);
-// doc.text('8', 300, 105);
+    doc.moveDown();
 
-doc.moveDown();
+    doc.fontSize(24);
+    var i;
+    for (i = 1; i <= 5; i++) {
+      doc.text('Team '+i.toString(), {
+        widh:410,
+        align:'center'
+      });
+    doc.addPage();
+    }
 
-doc.fontSize(24);
-var i;
-for (i = 1; i <= 5; i++) {
-  doc.text('Team '+i.toString(), {
-    widh:410,
-    align:'center'
-  });
-doc.addPage();
+
+
+
+    // end and display the document in the iframe to the right
+    doc.end();
+    stream.on('finish', function() {
+      location.href = stream.toBlobURL('application/pdf');
+    });
 }
-
-
-
-
-// end and display the document in the iframe to the right
-doc.end();
-stream.on('finish', function() {
-  location.href = stream.toBlobURL('application/pdf');
-});
