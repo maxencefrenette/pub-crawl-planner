24e73c6687c9f1f0b076e1c1dccf9cc9b481d5ac
Maxime Plante <maxime.pl@outlook.com>
2017-01-29 00:21:30 -0500

    Bad indentation solved

--- js/pdfGenerator.js
+++ js/pdfGenerator.js
@@ -1,37 +1,35 @@
 function pdfGenerator(numberOfTeams, locations, combinations) {
+    this.numberOfTeams = numberOfTeams;
+    this.numberOfLocations = numberOfLocations;
+    this.combinations = combinations;
+    var combinationsLength = 3; //TODO
 
-  this.numberOfTeams = numberOfTeams;
-  this.numberOfLocations = numberOfLocations;
-  this.combinations = combinations;
-  var combinationsLength = 3; //TODO
+    /// create a document and pipe to a blob
+    var doc = new PDFDocument();
+    var stream = doc.pipe(blobStream());
 
-  /// create a document and pipe to a blob
-  var doc = new PDFDocument();
-  var stream = doc.pipe(blobStream());
+    doc.fontSize(12);
+    // doc.text('8', 300, 105);
 
-  doc.fontSize(12);
-  // doc.text('8', 300, 105);
+    doc.moveDown();
 
-  doc.moveDown();
+    doc.fontSize(24);
+    var i;
+    for (i = 1; i <= 5; i++) {
+        doc.text('Team '+i.toString(), {
+            align:'center'
+        });
+        doc.moveDown(2);
+        doc.fontSize(12);
+        doc.text('8:05 - Gerts', {
+            align: 'left'
+        })
+        doc.addPage();
+    }
 
-  doc.fontSize(24);
-  var i;
-  for (i = 1; i <= 5; i++) {
-      doc.text('Team '+i.toString(), {
-          align:'center'
-      });
-      doc.moveDown(2);
-      doc.fontSize(12);
-      doc.text('8:05 - Gerts', {
-          align: 'left'
-      })
-      doc.addPage();
-  }
-
-  // end and display the document in the iframe to the right
-  doc.end();
-  stream.on('finish', function() {
-      location.href = stream.toBlobURL('application/pdf');
-  });
-
+    // end and display the document in the iframe to the right
+    doc.end();
+    stream.on('finish', function() {
+        location.href = stream.toBlobURL('application/pdf');
+    });
 }
